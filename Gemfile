62df74276531c5c1e30506693ef25aeedefff8df
marcelolara <chelop@gmail.com>
2013-11-06 18:08:28 -0300

    Adds AssetPack and Yui Compressor

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'haml'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'yui-compressor', :require => 'yui/compressor'
--- main.rb
+++ main.rb
@@ -1,7 +1,26 @@
 require 'sinatra/base'
+require 'sinatra/assetpack'
 require 'haml'
 
 class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  assets {
+
+    # Configure folder to save files
+    serve '/css', from: 'app/css'
+
+    # Group different files in one directive
+    css :application, '/css/application.css', [
+      '/css/reset.css',
+      '/css/layout.css'
+    ]
+
+    # Set file compression
+    css_compression :yui
+  }
 
   get '/' do 
     @title= "base-sinatra-modular-rack-haml-assetpack-example"
