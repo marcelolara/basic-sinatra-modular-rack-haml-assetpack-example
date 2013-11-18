82de743e9cc6595e05a021a346b8c8d83810e122
marcelolara <chelop@gmail.com>
2013-11-18 19:06:44 -0300

    Moves AssetPack init to website controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,4 @@ class ApplicationController < Sinatra::Base
   set :root, File.expand_path('../../',__FILE__)
 
   # puts "#{root}"
-
-  register Sinatra::AssetPack
-
-  assets do
-
-    # Configure folder to save files
-    serve '/css', from: 'assets/css'
-
-    # Group different files in one directive
-    css :application, '/css/application.css', [
-      '/css/reset.css',
-      '/css/layout.css'
-    ]
-
-    # Set file compression
-    css_compression :yui
-  end
 end
--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,12 +2,16 @@ class WebsiteController < ApplicationController
   register Sinatra::AssetPack
 
   assets do
+    # Configure folder to save files
     serve '/css', from: 'assets/css'
 
-    css:application, '/css/application.css', [
+    # Group different files in one directive
+    css :application, '/css/application.css', [
       '/css/reset.css',
       '/css/layout.css'
     ]
+
+    # Set file compression
     css_compression :yui
   end
 
